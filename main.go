f438c17e09d99b2b685d654392e30fe7aec189a3
@karantin2020 <karantin2020@users.noreply.github.com>
2017-06-17 13:05:31 +0500

    Restore

--- main.go
+++ main.go
@@ -53,17 +53,14 @@ func main() {
 	n.UseHandler(r)
 
 	// Start http server
-	err = http.ListenAndServe(env.Getenv("HYDRA_CLIENT_HOST", "localhost")+":4445", n)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Listening on "+env.Getenv("HYDRA_CLIENT_HOST", "localhost")+":4445")
+	http.ListenAndServe(":4445", n)
+	log.Println("Listening on :4445")
 }
 
 // handles request at /home - a small page that let's you know what you can do in this app. Usually the first.
 // page a user sees.
 func handleHome(w http.ResponseWriter, _ *http.Request) {
-	var authUrl = client.OAuth2Config("http://"+env.Getenv("HYDRA_CLIENT_HOST", "localhost")+":4445/callback", "offline", "openid").AuthCodeURL(state) + "&nonce=" + state
+	var authUrl = client.OAuth2Config("http://localhost:4445/callback", "offline", "openid").AuthCodeURL(state) + "&nonce=" + state
 	renderTemplate(w, "home.html", authUrl)
 }
 
@@ -184,7 +181,7 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 		// Redirect the user back to the consent endpoint. In a normal app, you would probably
 		// add some logic here that is triggered when the user actually performs authentication and is not
 		// part of the consent flow.
-		http.Redirect(w, r, "http://"+env.Getenv("HYDRA_CLIENT_HOST", "localhost")+":4445/consent?challenge="+challenge, http.StatusFound)
+		http.Redirect(w, r, "http://localhost:4445/consent?challenge="+challenge, http.StatusFound)
 		return
 	}
 
@@ -198,7 +195,7 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 	// in the real world you should check the state query parameter, but this is omitted for brevity reasons.
 
 	// Exchange the access code for an access (and optionally) a refresh token
-	token, err := client.OAuth2Config("http://"+env.Getenv("HYDRA_CLIENT_HOST", "localhost")+":4445/callback").Exchange(context.Background(), r.URL.Query().Get("code"))
+	token, err := client.OAuth2Config("http://localhost:4445/callback").Exchange(context.Background(), r.URL.Query().Get("code"))
 	if err != nil {
 		http.Error(w, errors.Wrap(err, "Could not exhange token").Error(), http.StatusBadRequest)
 		return
