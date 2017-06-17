0d51d52a0f9798cab42953ea5823a4492e28d3c8
@karantin2020 <karantin2020@users.noreply.github.com>
2017-06-17 08:50:40 +0500

    Added env HYDRA_CLIENT_CALLBACK

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 // handles request at /home - a small page that let's you know what you can do in this app. Usually the first.
 // page a user sees.
 func handleHome(w http.ResponseWriter, _ *http.Request) {
-	var authUrl = client.OAuth2Config("http://localhost:4445/callback", "offline", "openid").AuthCodeURL(state) + "&nonce=" + state
+	var authUrl = client.OAuth2Config(env.Getenv("HYDRA_CLIENT_CALLBACK", "http://localhost:4445/callback"), "offline", "openid").AuthCodeURL(state) + "&nonce=" + state
 	renderTemplate(w, "home.html", authUrl)
 }
 
